fc37be0c17a3240d8137ea777fb669ec28fdf80e
Asanka <asanka.jsndr@gmail.com>
2017-09-12 06:29:28 +0530

    twitter search results saving csv and loadDataSet from csv

--- Twitter sentiment analysis.py
+++ Twitter sentiment analysis.py
@@ -5,8 +5,17 @@ Created on Mon Sep 11 02:15:33 2017
 @author: Asanka
 """
 
+import pandas as pd
 import tweepy
 from textblob import TextBlob
+import numpy as np
+import csv
+
+# Open/create a file to append data to
+csvFile = open('./result.csv', 'a',newline='')
+
+#Use csv writer
+csvWriter = csv.writer(csvFile)
 
 # Authenticate
 consumer_key= 'Q9AsyQxjEMyS2uNEKccN2QFIV'
@@ -21,18 +30,21 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 #Step 3 - Retrieve Tweets
-public_tweets = api.search('Trump')
+public_tweets = api.search('Srilanka')
 
+print(type(public_tweets))
 
-
+csvWriter.writerow(['created_at','text'])
 for tweet in public_tweets:
     
     print(tweet.text ,'\n')
+    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
     
 #Step 4 Perform Sentiment Analysis on Tweets
-    analysis = TextBlob(tweet.text)
-    print(analysis.sentiment)
-    print("")
-    
-#in output polarity mesures how positive or negative some text is , 
-#subjectivity mesures how much of an opinion it is and how factual   
+    analysis = TextBlob(tweet.text)#in output polarity mesures how positive or negative some text is , #subjectivity mesures how much of an opinion it is and how factual 
+
+csvFile.close()
+
+
+dataSet=pd.read_csv('result.csv')
+dataSet.set_index('created_at')
